677a9ac5167531973da74db72ca15cd8bb99ffae
gparida1989 <gopalparida434@gmail.com>
2021-01-16 09:20:11 +0530

    16JanM

---
+++ ClassLibraryDemo/KeywordDemo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ClassLibraryDemo
+{
+    // private, public, protected, 
+    // by defaul class members are private
+    // internal, protected internal, private protected
+    class KeywordDemo
+    {
+        protected string Name = "indra";
+        protected int Amount = 65;
+        protected void Test()
+        {
+            int i = 6;
+            var name = true;  // explicit datatype declaration
+        }
+
+    }
+    class Another: KeywordDemo
+    {
+        
+        void TestA()
+        {
+           
+        }
+    }
+}
