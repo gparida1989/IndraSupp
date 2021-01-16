0883727e7a43d7928bda1a934954f9eeb1d6253b
gparida1989 <gopalparida434@gmail.com>
2021-01-16 20:19:00 +0530

    oopdemo

---
+++ ClassLibraryDemo/OOPSDemo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibraryDemo
+{
+    class OOPSDemo
+    {
+    }
+    // Class/struct - used to define templates fo objects
+    public class Bird
+    {
+        // 4 properties - Wings, body, 2-legs, Beak
+        public bool HasWings;
+        public bool HasBody;
+        public int NoOfLegs;
+        public bool HasBeak;
+
+        string Name;
+
+        public Bird(string birdName)
+        {
+            HasBeak = true;
+            HasBody = true;
+            HasWings = true;
+            NoOfLegs = 2;
+            Name = birdName;
+        }
+
+      
+        // ternary operator
+
+        // 1 behaviour - FLY
+        public void Fly()
+        {
+            if(Name==null)
+            {
+                Name = "No Name";
+            }
+           
+            Console.WriteLine("I am flying. & my name is " + Name);
+        }
+    }
+}
--- ConsoleDemo/Program.cs
+++ ConsoleDemo/Program.cs
@@ -8,9 +8,20 @@ namespace ConsoleDemo
         // entry point
         static void Main(string[] args)
         {
-            KeywordDemo obj = new KeywordDemo();
+            Bird bird1 = new Bird("indra");
 
-            
+            Console.WriteLine(bird1.HasBeak + "  Legs-  " + bird1.NoOfLegs);
+
+            bird1.Fly();
+
+            Bird bird2 = new Bird("Trump");
+
+            Console.WriteLine(bird2.HasBeak + "  Legs-  " + bird2.NoOfLegs);
+
+            bird2.Fly();
+
+            bird1.Fly();
+
         }
         
     }
