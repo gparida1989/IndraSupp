1d70b6bdd0c40e67c7cdf8d7fe02a8fd3cdb4962
gparida1989 <gopalparida434@gmail.com>
2021-03-22 09:23:05 +0530

    lambda

--- ConsoleDemo/Program.cs
+++ ConsoleDemo/Program.cs
@@ -8,239 +8,44 @@ namespace ConsoleDemo
 {
     class Program
     {
+        delegate int MyDel(int a, int b);
+
+        static int Sum(MyDel del)
+        {
+            return del(10, 100);
+        }
         // entry point
         static void Main(string[] args)
         {
-            //Bird bird1 = new Bird("indra");
-
-            //Console.WriteLine(bird1.HasBeak + "  Legs-  " + bird1.NoOfLegs);
-
-            //bird1.Fly();
-
-            //Bird bird2 = new Bird("Trump");
-
-            //Console.WriteLine(bird2.HasBeak + "  Legs-  " + bird2.NoOfLegs);
-
-            //bird2.Fly();
-
-            //bird1.Fly();
-
-            //AbstractDemo obj = new AbstractDemo();
-
-
-            //Console.WriteLine("The version is : " + obj.Version);
-
-            //var result = obj.Calculate(5, 8);
-            //Console.WriteLine("The version is : " + obj.Version);
-
-            //Console.WriteLine($"the result is : {result}");
-
-            //ArrayDemo o = new ArrayDemo();
-            //o.Test();
-
-
-
-            //var obj = new MethodDemo();
-            //var res= obj.Add(4,6.9f);
-
-            //Console.WriteLine(res);
-
-
-            //var obj1 = new KClass();  //a=12// instantiation , object == instance ,, a, inc() is called instance member
-            //Console.WriteLine($" value 1: {obj1.a}");//10
-            //obj1.Inc();//12
-            //Console.WriteLine($" value 1: {obj1.a}");//12
-            //obj1.Inc();//14
-
-            //var obj2 = new KClass();//a=10
-            //obj2.Inc();//12
-            //Console.WriteLine($" value 2 : {obj2.a}");//12
-
-
-            //Console.WriteLine($" value : {obj1.a}");// 14
-            ////obj1.Inc();
-
-            //Console.WriteLine($" value : {obj2.a}"); // 10
-
-            //var obj = new KClass();
-
-
-
-            //Console.WriteLine(KClass.SI);//10
-
-            //KClass.SInc();//11
-            //var o = new KeywordDemo();
-            //o.Test();//11
-            //obj.Inc();//a= 12,si =11
-            //KClass.SInc(); KClass.SInc(); KClass.SInc();//14
-            //Console.WriteLine(KClass.SI);//14
-            //o.Test();//14
-            //obj.Inc();//a=14,si=12
-            //obj.Inc();//a=16,si=13
-            //KClass.SInc(); KClass.SInc(); KClass.SInc();//16
-            //Console.WriteLine(KClass.SI);//16
-
-
-            //int a = 50;
-            //var res = a.Add1K(10);
-            //Console.WriteLine(res);
-
-            //string s = "indraa";
-            //char m = s.GetMiddleChar();
-
-            //Console.WriteLine(m);
-            //int[] ar = { 1,2,3,4,5};
-            //int sm = ar.IndexSum();
-            //Console.WriteLine(sm);
-
-            //var obj = new ArrayDemo();
-            //obj.Test1();
-
-            // (MyBase)b --> obj of mybase
-            //Demo b = new Demo();
-            //b.Test();
-
-
-            //var o = new IndraPrint();
-            //b.PP(o);
-
-            // Variable type : different than Instance type
-            // instance wuld be child of vaiable type
-            //Animal d1 = new Dog();
-            //d1.eat();
-            // d1.bark();
-
-            //Tabletop t = new Tabletop(4.5, 7.5);
-            //t.Display();
-
-            //StaticTest d = new StaticTest();
-
-            //d.Test();
-
-
-            //Console.Write("enter string : ");
-            //string s = Console.ReadLine();
-
-            //Console.Write("enter the charecter to search  :");
-            //char c = Convert.ToChar( Console.ReadLine());
-
-            //bool found = s.FindLtr(c);
-
-            //if (found == true) Console.WriteLine("found");
-            //else Console.WriteLine("not found");
-
-
-            // count the no of times the letter occurs in the  string
-            // function name : CountNumber
-            // extn against datatype : string
-            // parameter : char
-            // return type : int
-
-            // signature ???
-
-            //int r = s.CountNumber(c);
-
-            //Console.Write(r);
-
-
-            //var k = new KeywordDemo("canada");
-            //k.Test();
-
-            //var k1 = new KeywordDemo("mexico");
-
-            //k1.Test();
-            //typeof()
-            //sizeof()
-
-            //ternary op  -->   ?:
-
-            //is
-            //as
-
-            //int i = 100;
-
-            //if(i > 100)
-            //{
-            //    s="hiiii";
-            //}
-            //else
-            //{
-            //    s="hello";
-
-            //}
-
-            // s = (i>100) ? "hiiii" : "hello";
-
-            //if(i is int)
-            //{
-            //    Console.Write("yes it is string");
-            //}
-
-            //Console.Write(s);
-
-            //int e = Convert.ToInt32( Weeks.Friday);
-            //Console.WriteLine(e);
-
-            //MyClass k = new MyClass();
-            //MyClass.S1 = "hgh";
-
-
-            //MyStruct myStruct=new MyStruct();
-            //myStruct.print();
-            //MyStruct.S1 = "ddfddfd";
-
-
-            //FileSteamDemo o = new FileSteamDemo();
-            //o.ReadFile();
-
-            //ADODemo aDO = new ADODemo();
-            //aDO.FetchTableDisconnected();
-
-            //IndexerDemo n = new IndexerDemo("canada", "indra");
-            //var country = n[0];
-            //var name = n[1];
-
-            //Console.WriteLine($"country = {country}");
-
-            //CollectionDemo c
-            //    = new CollectionDemo();
-            //c.Test();
-
-            //int[] a = new int[8];
-            //foreach(var x in c)
-            //{
-
-            //}
-
-            //EnuDemo e = new EnuDemo();
-            //e.Reset();
-            //do
-            //{
-            //    Console.WriteLine(e.Current);
-            //} while (e.MoveNext());
-
-            //foreach(var o in e)
-            //{
-            //    Console.WriteLine(o);
-            //}
+           
 
             MyCass c = new MyCass();
 
             DelDemo d = new DelDemo();
 
-            DelDemo.indra mydel = c.Sing;
-            mydel += d.Funtus;
-            mydel += c.Sing1;
-            mydel += c.Sing2;
-            mydel += c.Sing3;
-            mydel += c.Sing4;
+            d.Test1(a => Console.Write("I am being passed as a param"));
 
-            Delegate[] result =  mydel.GetInvocationList();
 
-            for(int i = 0; i < result.Length; i++)
+            int result = Sum(delegate (int a, int b)
             {
-                Console.WriteLine(result[i].Method.Name);
-            }
+                return a+b;
+            });
+
+            Console.Write("resut ins " + result);
+
+            //DelDemo.indra mydel = c.Sing;
+            //mydel += d.Funtus;
+            //mydel += c.Sing1;
+            //mydel += c.Sing2;
+            //mydel += c.Sing3;
+            //mydel += c.Sing4;
+
+            //Delegate[] result =  mydel.GetInvocationList();
+
+            //for(int i = 0; i < result.Length; i++)
+            //{
+            //    Console.WriteLine(result[i].Method.Name);
+            //}
 
             //Random random = new Random(0);
 
@@ -264,11 +69,6 @@ namespace ConsoleDemo
             //    Console.Write($"{i} => ");
             //    Console.WriteLine(d.Compute($"{i}"));
             //}
-
-
-
-
-
 
 
 
